e7d85ed002917b6c18400e32a34fab509d122a9a
daeun <58421346+qor4@users.noreply.github.com>
2022-10-17 18:14:33 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -13,8 +13,9 @@ import os
 from urllib.request import urlopen
 import uuid
 
-driver = webdriver.Chrome("C:/Users/eunseo/AppData/Local/Temp/BNZ.633853164ec41f/chromedriver.exe")
-regions = ['경기도', '경상북도', '경상남도', '서울특별시', '부산광역시', '대구광역시', '인천광역시', '광주광역시', '대전광역시', '울산광역시', '세종특별자치시', '강원도', '충청북도', '충청남도', '전라북도', '전라남도', '제주특별자치도']
+driver = webdriver.Chrome("C:\\Users\\qor_4\\Downloads\\chromedriver_win32\\chromedriver.exe")
+regions = ['경기도', '경상북도', '경상남도', '서울특별시', '부산광역시', '대구광역시', '인천광역시', '광주광역시', '대전광역시', '울산광역시', '세종특별자치시', '강원도',
+           '충청북도', '충청남도', '전라북도', '전라남도', '제주특별자치도']
 
 driver.get('https://korean.visitkorea.or.kr/list/ms_list.do?choiceTag=%EC%97%AC%ED%96%89%EC%A7%80&choiceTagId=')
 # driver.get('https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid=9d506d09-62ec-40d4-87a2-a0fd35b25b80&big_category=A02&mid_category=A0201&big_area=35'
@@ -23,42 +24,63 @@ region = []
 content = []
 imagelist = []
 address = []
-lat=[]
-lon=[]
+lat = []
+lon = []
 images = []
 
-for k in range(6, 9):
-    print(k)
-    v = "//*[@id='" + str(k) + "']"
+# 6페이지까지 넘어가는 코드
+v = "/ html / body / div[2] / div[2] / div[1] / div[2] / a[6]"
+myInputElm = driver.find_element(by=By.XPATH, value=v)
+myInputElm.click()
+
+# 내가 처음 가져올 페이지 // 5 - 1?
+# for page in range(11, 17, 5):
+#     v = "/ html / body / div[2] / div[2] / div[1] / div[2] / a[8]"
+#     myInputElm = driver.find_element(by=By.XPATH, value=v)
+#     myInputElm.click()
+
+for k in range(6, 12):
+    print("현재 페이지 수:",k)
+    if k%5==0:
+        v = "/ html / body / div[2] / div[2] / div[1] / div[2] / a[7]"
+    else:
+        v = "/ html / body / div[2] / div[2] / div[1] / div[2] / a[" + str(k % 5 + 2) + "]"
+
+    # / html / body / div[2] / div[2] / div[1] / div[2] / a[1]
+    # / html / body / div[2] / div[2] / div[1] / div[2] / a[2]
+    # v="//*[@id='"+str(k)+"']"
     myInputElm = driver.find_element(by=By.XPATH, value=v)
     myInputElm.click()
     html = driver.page_source
+    print(html)
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup)
-    for ttmp in range(1, 2):
-        va = "//*[@id='contents']/div[2]/div[1]/ul/li["+str(1)+"]/div[2]/div/a"
-        driver.find_element(by=By.XPATH, value=va).send_keys(Keys.ENTER) #xpath값 클릭하기
+
+    # print(soup)
+    for data in range(1, 11):
+        # //*[@id="contents"]/div[2]/div[1]/ul/li[1]/div[2]/div/a
+        # //*[@id="contents"]/div[2]/div[1]/ul/li[3]/div[2]/div/a
+        # //*[@id="contents"]/div[2]/div[1]/ul/li[10]/div[2]/div/a
+        va = "//*[@id='contents']/div[2]/div[1]/ul/li[" + str(data) + "]/div[2]/div/a"
+        driver.find_element(by=By.XPATH, value=va).send_keys(Keys.ENTER)  # xpath값 클릭하기
         time.sleep(2)
-        html = driver.page_source ## 페이지의 elements모두 가져오기
-        soup = BeautifulSoup(html, 'html.parser') ## BeautifulSoup사용하기
+        html = driver.page_source  ## 페이지의 elements모두 가져오기
+        soup = BeautifulSoup(html, 'html.parser')  ## BeautifulSoup사용하기
 
         elements = soup.select('h2#topTitle')
         elements.extend((soup.select('div.area_address')))
         elements.extend((soup.select('div.inr>p')))
-
 
         travel = []
 
         for j in elements:
             travel.append(j.text)
 
-        image = soup.find("img", class_="swiper-lazy swiper-lazy-loaded").get('src')
-        images.append(image)
+        image = soup.find("img", class_="swiper-lazy swiper-lazy-loaded")
+        images.append(image.get('src'))
         # images = []
         # for j in range(len(image)):
         #     image = soup.find("img", class_="swiper-lazy swiper-lazy-loaded")
         #     images.append(image.get('src'))
-        
 
         add = soup.select('div.inr>ul>li>span')
         for j in add:
@@ -66,10 +88,11 @@ for k in range(6, 9):
             if tmp[0] in regions:
                 travel.append(j.text)
 
-        time.sleep(2)
+        # time.sleep(2)
         # print(tmp)
         print("ss", travel)
         api_key = 'b821e0860b165d2a52fe06d433d69e03'
+
 
         def addr_to_lat_lon(addr):
             url = 'https://dapi.kakao.com/v2/local/search/address.json?query={address}'.format(address=addr)
@@ -97,24 +120,14 @@ for k in range(6, 9):
 
         driver.execute_script("window.history.go(-1)")
         driver.refresh()
-data = pd.DataFrame(zip(name, region, content, address,lat, lon), columns=['name', 'location', 'info', 'address', 'latitude', 'longitude'])
+        time.sleep(2)
 
-data.to_csv('C:/Users/eunseo/Desktop/study/여행지 지도.csv', index=False, encoding='cp949')
+# data = pd.DataFrame(zip(name, region, content, address, lat, lon),
+#                     columns=['name', 'location', 'info', 'address', 'latitude', 'longitude'])
 #
-# # url = 'https://search.naver.com/search.naver?where=post&sm=tab_jum&query='+search
-# # driver.get(url)
-# # html = driver.page_source ## 페이지의 elements모두 가져오기
-# # soup = BeautifulSoup(html, 'html.parser') ## BeautifulSoup사용하기
-# #
-# # elements = soup.select('._2yqUQ')
-# # # print(soup)
-# # arr = []
-# # for i in elements:
-# #     arr.append(i.text)
-# #
-# # print(arr)
-#
-# driver.close()
+# data.to_csv('./여행지 지도.csv', index=False, encoding='cp949')
+
+
 
 
 def s3_connection():
@@ -129,8 +142,9 @@ def s3_connection():
     except Exception as e:
         print(e)
     else:
-        print("s3 bucket connected!") 
+        print("s3 bucket connected!")
         return s3
+
 
 def saveImageToS3(travelImg):
     s3 = s3_connection()
@@ -150,30 +164,31 @@ def saveImageToS3(travelImg):
     return travelImgInS3
 
 
-# DB 저장 
-travelImgInS3 = saveImageToS3(images)
+# DB 저장
+# travelImgInS3 = saveImageToS3(images)
 awsHost = "13.124.150.86"
 loclaHost = "127.0.0.1"
 conn = pymysql.connect(host=awsHost, user='ec2-user', password='root', db='nadri_gil', charset='utf8');
 
 cur = conn.cursor();
-id=0
-for i in range(0,len(name)):
+id = 50
+for i in range(0, len(name)):
     name1 = name[i]
     findSql = f"SELECT * FROM travel WHERE name = '{name1}'"
     cur.execute(findSql)
     count = cur.rowcount
-    if(count == 0):
+    if (count == 0):
         region1 = region[i]
         content1 = content[i]
         address1 = address[i]
+        image1 = images[i]
         lat1 = lat[i]
         lon1 = lon[i]
         created_date = datetime.now()
         last_modified_date = datetime.now()
-        id+=1
-        imageUrl = travelImgInS3[i]
-        saveSql = f"INSERT INTO travel (travel_id, name, location, info, address, latitude, longitude, created_date, last_modified_date, image) VALUES ({id},'{name1}', '{region1}' ,'{content1}','{address1}', '{lat1}','{lon1}', '{created_date}', '{last_modified_date}', '{imageUrl}');"
+        id += 1
+        # imageUrl = travelImgInS3[i]
+        saveSql = f"INSERT INTO travel (travel_id, name, location, info, address, latitude, longitude, created_date, last_modified_date, image) VALUES ({id},'{name1}', '{region1}' ,'{content1}','{address1}', '{lat1}','{lon1}', '{created_date}', '{last_modified_date}', '{image1}');"
         cur.execute(saveSql)
         conn.commit()
 conn.close()
